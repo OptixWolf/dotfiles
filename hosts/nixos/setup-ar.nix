ba8645665ae067e56217955be35e95d8eef27582
OptixWolf <62619140+OptixWolf@users.noreply.github.com>
2026-09-15 07:57:34 +0200

    Added intel modules for setup-ar

--- hosts/nixos/setup-ar.nix
+++ hosts/nixos/setup-ar.nix
@@ -3,4 +3,8 @@
   networking.hostName = "NixOS-Arbeitsrechner";
 
   services.xserver.videoDrivers = [ "modesetting" ];
+
+  boot.initrd.availableKernelModules = [ "vmd" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  hardware.cpu.intel.updateMicrocode = true;
 }
